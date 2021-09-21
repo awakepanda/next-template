57b6cf6f1dfc4b32ce08e3e3a59b3d4e50c25fdc
symano <coloponpan@gmail.com>
2021-09-22 05:54:04 +0900

    🚧 mapで回していたリンクを一旦タグに置き換え。

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,44 @@
 import Link from "next/link";
 
-const NAV_ITEMS = [
-  { href: "/", label: "Index" },
-  { href: "/message", label: "Message" },
-  { href: "/about", label: "About" },
-  { href: "/team", label: "Team" },
-  { href: "/contact", label: "Contact" },
-];
+// const NAV_ITEMS = [
+//   { href: "/", label: "Index" },
+//   { href: "/message", label: "Message" },
+//   { href: "/about", label: "About" },
+//   { href: "/team", label: "Team" },
+//   { href: "/contact", label: "Contact" },
+// ];
 
 export const Header = () => {
   return (
     <header className="h-20 bg-gray-50">
       <div className="flex h-full">
+        <Link href="/">
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
+            Home
+          </a>
+        </Link>
+        <Link href="/about">
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
+            About
+          </a>
+        </Link>
+        <Link href="/message">
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
+            Message
+          </a>
+        </Link>
+        <Link href="/team">
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
+            Team
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
+            Contact
+          </a>
+        </Link>
+      </div>
+      {/* <div className="flex h-full">
         {NAV_ITEMS.map((item) => {
           return (
             <Link key={item.href} href={item.href}>
@@ -21,7 +48,7 @@ export const Header = () => {
             </Link>
           );
         })}
-      </div>
+      </div> */}
     </header>
   );
 };
