cf65e0a6ac0012432e2f8c1ab6aadff8436f6b85
symano <coloponpan@gmail.com>
2021-09-22 06:52:27 +0900

    💄 Headerを上部固定に変更。

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 // const NAV_ITEMS = [
 //   { href: "/", label: "Index" },
@@ -10,32 +10,22 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="h-20 bg-gray-50">
+    <header className="w-full h-20 bg-gray-50 fixed left-0 top-0">
       <div className="flex h-full">
         <Link href="/">
-          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
-            Home
-          </a>
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">Home</a>
         </Link>
         <Link href="/about">
-          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
-            About
-          </a>
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">About</a>
         </Link>
         <Link href="/message">
-          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
-            Message
-          </a>
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">Message</a>
         </Link>
         <Link href="/team">
-          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
-            Team
-          </a>
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">Team</a>
         </Link>
         <Link href="/contact">
-          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">
-            Contact
-          </a>
+          <a className="w-1/5 flex justify-center items-center hover:bg-black hover:text-white transition">Contact</a>
         </Link>
       </div>
       {/* <div className="flex h-full">
