bc3a4d6c9b84934719acbfb3304962147cf492ac
symano <coloponpan@gmail.com>
2021-09-22 05:48:14 +0900

    💄 prettierの設定ファイルを追加。Tailwind CSSのfontFamilyをNoto Sans JPに設定。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  mode: "jit",
+  mode: 'jit',
   purge: ['src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans JP'],
+        // display: ['Noto Sans JP'],
+        // body: ['Noto Sans JP'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
