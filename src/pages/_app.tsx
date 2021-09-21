9b11a2ed73c981721a046f550ba6480d4dacf6f9
symano <coloponpan@gmail.com>
2021-09-22 06:44:53 +0900

    🎨 Bodyタグにclassを付与。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-// import "src/styles/globals.css";
-import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
+// import 'src/styles/globals.css';
+import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
+// import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // <>
+  //   <Head>
+  //     <meta
+  //       name="viewport"
+  //       content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  //     />
+  //   </Head>
+  //   <Component {...pageProps} />;
+  // </>;
   return <Component {...pageProps} />;
 }
 export default MyApp;
