7b6a4968f3f5153cafa9e5eed54c21a693edc95b
symano <coloponpan@gmail.com>
2021-09-26 09:38:35 +0900

    ⚡️ eslintの設定追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,14 @@ class Document extends NextDocument<Props> {
   render() {
     return (
       <Html lang="ja">
+        <img src="" alt="" />
         <Head>
-          {/* <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet"></link> */}
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/yakuhanjp@3.4.1/dist/css/yakuhanjp-noto.min.css"
           ></link>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
